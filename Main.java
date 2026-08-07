6d2a5b63c929766fe9aacb59c24b9738056aec58
nagashimakaito <kit.nagashima@symmetrix.co.jp>
2026-08-07 15:58:20 +0900

    Javaの練習コード追加

---
+++ Main.java
@@ -0,0 +1,77 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("すがわら");
+        System.out.println("31歳です。");
+        System.out.println("お酒が大好きです！");
+        System.out.println("31 + 31 の計算をします。");
+        System.out.println(31 + 31);
+
+
+        System.out.println(35 -10);
+        System.out.println(-5 * 2);
+        int x;
+        x = 6;
+        System.out.println(x * x * 3.14);
+        System.out.println("答えは " + 64);
+
+        System.out.println("RPG; スッキリ魔王討伐");
+        System.out.println("Ver.0.1 by 渚");
+        System.out.println("<ただいま鋭意学習中>");
+        System.out.println("プログラムを終了します");
+
+        /*
+
+        複数行
+    
+        */
+
+        //単数行
+
+        int age;
+        age = 30;
+        System.out.println(age);
+
+        byte glasses; glasses = 2;
+        short ages; ages = 18;
+        int salary; salary = 152000;
+        long worldPeople; worldPeople = 6900000000L;
+
+        System.out.println(glasses);
+        System.out.println(ages);
+        System.out.println(salary);
+        System.out.println(worldPeople);
+
+
+        float weight; weight = 67.5F;
+        double height; height = 171.2;
+
+        System.out.println(weight);
+        System.out.println(height);
+
+
+        boolean isError; isError = true;
+        System.out.println(isError);
+        boolean result; result =false;
+        System.out.println(result);
+
+        char zodiac; zodiac = '辰';
+        System.out.println(zodiac);
+        String name; name = "すがわら";
+        System.out.println(name);
+
+
+        age = 20;
+        System.out.println("私の年齢は" + age);
+        age = 31;
+        System.out.println("...いや、本当の年齢は" + age);
+
+        final double PI = 3.14;
+        int pie = 5;
+        System.out.println("半径" + pie + "cmパイの面積は、");
+        System.out.println(pie * pie * PI);
+        System.out.println("パイの半径を倍にします");
+        pie = 10;
+        System.out.println("半径" + pie + "cmパイの面積は、");
+        System.out.println(pie * pie * PI);
+    }
+}
---
+++ math.java
@@ -0,0 +1,58 @@
+public class math {
+    public static void main(String[] arges){
+        int a = 20;
+        int b = a + 5;
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("私の好きな記号は二重引用符(\")です。");
+
+        a = 100;
+        a++;
+        System.out.println(a);
+
+        float f = 3;
+        double d = f;
+        System.out.println(f);
+        System.out.println(d);
+        
+        int age = (int)3.2;
+        System.out.println(age);
+
+        d= 8.5 / 2;
+        long l = 5 + 2L;
+        System.out.println(d);
+        System.out.println(l);
+
+        String msg = "私の年齢は" + 23;
+        System.out.println(msg);
+
+        String name = "すがわら";
+        String message;
+        message = name + "さん、こんにちは";
+        System.out.println(message);
+
+        String name1 = "すがわら";
+        System.out.print("私の名前は");
+        System.out.print(name1);
+        System.out.println("です。");
+
+        a = 5;
+        b = 3;
+        int m = Math.max(a,b);
+        System.out.println("比較実験：" + a + "と" + b + "の大きい方は" + m + "です。");
+
+        String ageText = "31";
+        int n = Integer.parseInt(ageText);
+        System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+
+        int r = new java.util.Random().nextInt(90);
+        System.out.println("あなたはたぶん、" + r + "歳ですね？");
+
+        System.out.println("あなたの名前を入力してください。");
+        String userName = new java.util.Scanner(System.in).nextLine();
+        System.out.println("あなたの年齢を入力してください。");
+        int userAge = new java.util.Scanner(System.in).nextInt();
+        System.out.println("ようこそ、" + userAge + "歳の" + userName + "さん。");
+    }
+}
